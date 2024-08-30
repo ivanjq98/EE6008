b40296420d61ad84876e61c302b882b737e28f9f
ivanjq98 <tan.ivancjq@gmail.com>
2024-08-31 01:37:16 +0800

    Delete-a-column-named-rows

---
+++ prisma/migrations/20240830173425_delete_roles_model_faculty/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `Faculty` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Faculty" DROP COLUMN "role";
